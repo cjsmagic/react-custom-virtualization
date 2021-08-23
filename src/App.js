e5fd991a1d0822fb8a1ef12fca9367678cae387b
cjsmagic <clarencemerchant1995@gmail.com>
2021-08-23 07:40:35 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import VirtualList from './VirtualList';
 
-const items = new Array(100).fill(null).map((_, index) => `item_${index + 1}`);
+const newItems = count =>
+  new Array(count).fill(null).map((_, index) => `item_${index + 1}`);
 
 const renderItem = ({ item, index }) => (
   <div
     style={{
-      background: index % 2 === 0 ? 'lightblue' : 'lightgreen',
+      background: index % 2 === 0 ? 'lightblue' : '#000',
+      color: index % 2 === 0 ? '#000' : '#fff',
       height: '50px',
       display: 'flex',
       alignItems: 'center',
@@ -20,16 +22,53 @@ const renderItem = ({ item, index }) => (
   </div>
 );
 
+const initialCount = 100;
+
 export default function App() {
+  const [list, setList] = useState(() => newItems(initialCount));
+  const [count, setCount] = useState(() => initialCount);
+  const [indexPointed, setIndexPointed] = useState(0);
+
   return (
     <div>
-      <h1>Virtual List for {items.length} items</h1>
+      <h1>Virtual List for {list.length} items</h1>
+
+      <input
+        type="number"
+        value={count}
+        onChange={e => setCount(e.target.value)}
+      />
+
+      <button
+        onClick={() => {
+          setList(newItems(parseInt(count)));
+        }}
+      >
+        Update items
+      </button>
+
+      <button onClick={() => setList(['test', ...list])}>
+        change random item
+      </button>
+
+      <div>
+        change index
+        <select
+          value={indexPointed}
+          onChange={e => setIndexPointed(parseInt(e.target.value))}
+        >
+          {list.map((item, index) => (
+            <option value={index}>{item}</option>
+          ))}
+        </select>
+      </div>
 
       <VirtualList
-        items={items}
+        items={list}
         renderItem={renderItem}
-        height={250}
+        height={500}
         rowSize={50}
+        indexPointed={indexPointed}
       />
     </div>
   );
--- src/VirtualList.js
+++ src/VirtualList.js
@@ -1,74 +1,98 @@
 import Proptypes from 'proptypes';
-import React, { useRef, useLayoutEffect, useState, useMemo } from 'react';
+import React, { useRef, useEffect, useState, useMemo } from 'react';
+
+const defaultProps = {
+  indexPointed: 0
+};
 
 const proptypes = {
   items: Proptypes.array.required,
   renderItem: Proptypes.func.required,
   height: Proptypes.number.required,
-  rowSize: Proptypes.number.required
+  rowSize: Proptypes.number.required,
+  indexPointed: Proptypes.string.required
 };
 
-function VirtualList({ items, renderItem, height, rowSize }) {
+function VirtualList({ items, renderItem, height, rowSize, indexPointed }) {
   const ref = useRef();
 
   const [state, setState] = useState({
     currentIndex: 0,
     scrollTop: 0
   });
+
   const { currentIndex, scrollTop } = state;
 
   const rows = useMemo(() => {
-    return new Array(Math.ceil(height / rowSize)).fill(null).map((_, index) => {
-      // this should give us item based on array index and currentIndex
-      const item = items[index + currentIndex];
-      return (
-        <div
-          key={index}
-          className="virtual-list__item"
-          style={{
-            height: `${rowSize}px`,
-            top: `${scrollTop + index * rowSize}px`
-          }}
-        >
-          {renderItem({ item, index: index + currentIndex })}
-        </div>
-      );
-    });
+    return new Array(Math.round(height / rowSize))
+      .fill(null)
+      .map((_, index) => {
+        // this should give us item based on array index and currentIndex
+        const item = items[index + currentIndex];
+
+        if (!item) {
+          return null;
+        }
+
+        return (
+          <div
+            key={index}
+            className="virtual-list__item"
+            style={{
+              height: `${rowSize}px`,
+              transform: `translate3d(0,${index * rowSize + scrollTop}px,0)`
+            }}
+          >
+            {renderItem({ item, index: index + currentIndex })}
+          </div>
+        );
+      });
+    // .filter(item => item);
   }, [height, rowSize, items, scrollTop, currentIndex]);
 
-  useLayoutEffect(() => {
-    let oldIndex = 0;
-
-    const scrollEventHandler = e => {
-      /* 
-      a single scroll should cross 50px to make on scroll unit of 1,
-      when scrollTop = 50, and rowSize = 50, 50/50 should equal to 1, this will tell us the current index, also we got to avoid calculating for same index due to extra events fired
-      */
-      const _currentIndex = Math.ceil(e.target.scrollTop / rowSize);
-      if (oldIndex === _currentIndex) {
-        return;
-      }
-      oldIndex = _currentIndex;
-
+  useEffect(() => {
+    if (ref.current) {
+      // if items length is less than previous chosen index we need to make 0 as  index
+      const chosenIndex = indexPointed > items.length ? 0 : indexPointed;
+      ref.current.scrollTop = chosenIndex * rowSize;
       setState(prev => ({
         ...prev,
-        currentIndex: _currentIndex,
-        scrollTop: e.target.scrollTop
+        currentIndex: chosenIndex,
+        scrollTop: ref.current.scrollTop
       }));
+    }
+  }, [items.length, indexPointed]);
+
+  useEffect(() => {
+    const scrollEventHandler = e => {
+      const id = window.requestAnimationFrame(function() {
+        //if diff of scroll and rowsize is greater than scroll value of 10 we can ignore update
+        if (Math.ceil(e.target.scrollTop % rowSize) > 10) {
+          return;
+        }
+        setState(() => ({
+          currentIndex: Math.floor(e.target.scrollTop / rowSize),
+          scrollTop: e.target.scrollTop
+        }));
+        cancelAnimationFrame(id);
+      });
     };
 
     if (ref.current) {
       ref.current.style.height = `${height}px`;
       ref.current.querySelector(
         '.virtual-list__scroll'
-      ).style.minHeight = `${rowSize * items.length - rowSize}px`;
-      ref.current.addEventListener('scroll', scrollEventHandler);
+      ).style.minHeight = `${rowSize * items.length}px`;
+      ref.current.addEventListener('scroll', scrollEventHandler, {
+        passive: true
+      });
     }
 
     return () => {
+      console.log('effect destroyed');
       ref.current.removeEventListener('scroll', scrollEventHandler);
     };
-  }, [ref.current, setState]);
+  }, [ref.current, setState, items]);
 
   return (
     <div className="virtual-list" ref={ref}>
@@ -77,6 +101,7 @@ function VirtualList({ items, renderItem, height, rowSize }) {
   );
 }
 
+VirtualList.defaultProps = defaultProps;
 VirtualList.proptypes = proptypes;
 
 export default VirtualList;
